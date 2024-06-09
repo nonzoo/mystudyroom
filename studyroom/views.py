01f7483485821396d55bf5ed02b41cc31aa80fbd
nonzoo <nonsoosadebe202@gmail.com>
2024-06-09 22:37:17 +0100

    update and create room styled

--- studyroom/views.py
+++ studyroom/views.py
@@ -134,12 +134,17 @@ def create_room(request):
     form = RoomForm()
     topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)#get or create a new topic
+
+        Room.objects.create(
+            host= request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description')
+        )
+
+        return redirect('home')
     context = {'form':form,'topics':topics}
     return render(request, 'studyroom/room_form.html', context)
 
@@ -149,17 +154,22 @@ def create_room(request):
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance= room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('You are not allowed here!!  ')
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room )
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
         
-    context={'form':form}
+    context={'form':form,'topics':topics,'room':room}
 
     return render (request, 'studyroom/room_form.html', context  )
 
